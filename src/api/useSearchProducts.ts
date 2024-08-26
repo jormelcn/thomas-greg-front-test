9ec2325bb32cf7c2cf2699dfc07beb2ddebf1877
Jorge Narvaez <jormelcn@gmail.com>
2024-08-26 05:26:06 -0500

    update useSearchProducts

--- src/api/useSearchProducts.ts
+++ src/api/useSearchProducts.ts
@@ -1,5 +1,6 @@
 import { useMemo } from "react";
 import useSWR from "swr";
+import { FailedResponse } from "./ApiClient/GenericApiResponse";
 import { AdminSearchProductsRequestDTO } from "./customDTOs";
 import { PageDTOProductForAdminDTO } from "./dtos";
 import { useStoreService } from "./useStoreService";
@@ -7,5 +8,8 @@ import { useStoreService } from "./useStoreService";
 export function useSearchProducts(req: AdminSearchProductsRequestDTO) {
   const service = useStoreService();
   const url = useMemo(() => service.searchProductsUrl(req), [req, service]);
-  return useSWR(url, service.swrFetcher<PageDTOProductForAdminDTO>());
+  return useSWR<PageDTOProductForAdminDTO, FailedResponse>(
+    url,
+    service.swrFetcher()
+  );
 }
